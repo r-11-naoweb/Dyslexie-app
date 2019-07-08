95061db80cbb1f9625fcac278627bdc7f74e713a
Gwénola ETHEVE <ethevegwenola@gmail.com>
2019-07-08 19:21:43 +0200

    main activity

--- app/src/main/java/com/imie/dyslexie_app/MainActivity.java
+++ app/src/main/java/com/imie/dyslexie_app/MainActivity.java
@@ -10,5 +10,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Ouvre l'activité contenant le texte de correction
+        Button explicationButton = (Button) this.findViewById(R.id.button);
+        explicationButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent correctionActivity = new Intent(MainActivity.this, CorrectionActivity.class);
+                startActivity(correctionActivity);
+            }
+        });
+
+        // Lance la récitation du google Home
+        ImageButton googleHomeButton = (ImageButton) this.findViewById(R.id.imageButton);
+        explicationButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        // Lance l'appel à l'Api de correction
+        ImageButton correctionButton = (ImageButton) this.findViewById(R.id.imageButton3);
+        explicationButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
     }
 }
