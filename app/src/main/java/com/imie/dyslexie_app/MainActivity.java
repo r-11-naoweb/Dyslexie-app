fa5eb25fef6ff12795e562c822499f5df99131b5
Gwénola ETHEVE <ethevegwenola@gmail.com>
2019-07-09 15:08:47 +0200

    correction activity

--- app/src/main/java/com/imie/dyslexie_app/MainActivity.java
+++ app/src/main/java/com/imie/dyslexie_app/MainActivity.java
@@ -1,8 +1,13 @@
 package com.imie.dyslexie_app;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,38 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Ouvre l'activité contenant le texte de correction
-        Button explicationButton = (Button) this.findViewById(R.id.button);
-        explicationButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent correctionActivity = new Intent(MainActivity.this, CorrectionActivity.class);
-                startActivity(correctionActivity);
-            }
-        });
 
+        // Ouvre l'activité contenant le texte de correction
+        Button explicationButton = (Button) this.findViewById(R.id.explication);
         // Lance la récitation du google Home
         ImageButton googleHomeButton = (ImageButton) this.findViewById(R.id.imageButton);
-        explicationButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-
-            }
-        });
-
         // Lance l'appel à l'Api de correction
         ImageButton correctionButton = (ImageButton) this.findViewById(R.id.imageButton3);
-        explicationButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
 
-            }
-        });
+    }
+
+    public void onClick(View v)
+    {
+        final int id = v.getId();
+        switch (id){
+            case R.id.explication:
+                Intent correctionActivity = new Intent(MainActivity.this, CorrectionActivity.class);
+                startActivity(correctionActivity);
+                break;
+        }
+
     }
 }
