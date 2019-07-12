41c1ac250fab96d8548bcf9b2456b0983e58a332
bethuel <nicolasbethuel@gmail.com>
2019-07-12 02:07:28 +0200

    Add vocal button to correction screen

--- app/src/main/java/com/imie/dyslexie_app/CorrectionActivity.java
+++ app/src/main/java/com/imie/dyslexie_app/CorrectionActivity.java
@@ -1,13 +1,14 @@
 package com.imie.dyslexie_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,7 +22,7 @@ public class CorrectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_correction);
 
-        TextView explicationTextView = (TextView) findViewById(R.id.explicationText);
+        TextView explicationTextView = (TextView) findViewById(R.id.explanationText);
         explicationTextView.setMovementMethod(new ScrollingMovementMethod());
         String[] nameArray = {"Correction 1","Correction 2","Correction 3","Correction 4","Correction 5","Correction 6" };
 
@@ -50,5 +51,31 @@ public class CorrectionActivity extends AppCompatActivity {
             textView.setText(str);
         }
 
+    }
+
+    public void onClick(View v)
+    {
+        final int id = v.getId();
+        switch (id){
+            case R.id.vocalButton:
+                // Lance la lecture par google assistant
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                TextView errorWordTw = findViewById(R.id.errorWord);
+                String errorWord = errorWordTw.getText().toString();
+                TextView correctionWordTw = findViewById(R.id.correctionWord);
+                String correctionWord = correctionWordTw.getText().toString();
+                TextView explanationTextTw = findViewById(R.id.explanationText);
+                String explanationText = explanationTextTw.getText().toString();
+                String sentence = explanationSentence(errorWord, correctionWord, explanationText);
+
+                ClipData clip = ClipData.newPlainText("lecture_explication", sentence);
+                clipboard.setPrimaryClip(clip);
+                startActivity(new Intent(Intent.ACTION_VOICE_COMMAND).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                break;
+        }
+    }
+
+    public String explanationSentence(String errorWord, String correctionWord, String explanation) {
+        return String.format("Le mot %s doit s'écrire %s parce que %s", errorWord, correctionWord, explanation);
     }
 }
